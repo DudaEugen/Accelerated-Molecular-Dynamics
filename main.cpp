7f9c14c3b8c9af930dfea863dec1c3dcda2ca78d
DudaEugen <54473424+DudaEugen@users.noreply.github.com>
2019-11-20 10:47:43 +0200

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <cstddef>
+#include <ctime>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
