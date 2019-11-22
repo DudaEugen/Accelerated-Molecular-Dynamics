7066eb6a2d8cbae7d28170c1c556b64da92b6c8c
DudaEugen <54473424+DudaEugen@users.noreply.github.com>
2019-11-22 12:19:17 +0200

    add mpi library. Initialize and finalize streams

--- main.cpp
+++ main.cpp
@@ -5,10 +5,18 @@
 #include <cstddef>
 #include <ctime>
 #include <stdlib.h>
+#include "mpi.h"
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
+	int ProcRank, ProcNum;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+	
+	MPI_Finalize();
+
 	return 0;
 }
