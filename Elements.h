cd1e9ce174f888144d507071815a6aa5467cf905
PolyakAnton <54473424+PolyakAnton@users.noreply.github.com>
2019-11-26 23:58:34 +0200

    update enum class element - add Si - numbers are atomic number - add function get_atomic_number - add function overloadings for string argument

--- Elements.h
+++ Elements.h
@@ -2,16 +2,19 @@
 #ifndef TAHD_ELEMENTS_H
 #define TAHD_ELEMENTS_H
 
+#include <string>
+
 enum class element {
-	C,
-	Al,
-	Ar,
-	Ti,
-	Fe,
-	Ni,
-	Cu,
-	Ag,
-	Au
+	C  = 6,
+	Al = 13,
+	Si = 14,
+	Ar = 18,
+	Ti = 22,
+	Fe = 26,
+	Ni = 28,
+	Cu = 29,
+	Ag = 47,
+	Au = 79
 };
 
 element convert_to_element(char title)
@@ -31,11 +34,25 @@ element convert_to_element(char title[2])
 	else if (title[0] == 'A' && title[1] == 'u') { return element::Au; }
 }
 
-int get_atom_weight(element el)
+element convert_to_element(std::string title)
+{
+		 if (title == "C")  { return element::C;  }
+	else if (title == "Al") { return element::Al; }
+	else if (title == "Ar") { return element::Ar; }
+	else if (title == "Ti") { return element::Ti; }
+	else if (title == "Fe") { return element::Fe; }
+	else if (title == "Ni") { return element::Ni; }
+	else if (title == "Cu") { return element::Cu; }
+	else if (title == "Ag") { return element::Ag; }
+	else if (title == "Au") { return element::Au; }
+}
+
+int get_atomic_mass(element el)
 {
 		 if (el == element::C)  { return 12; }
 	else if (el == element::Al)	{ return 27; }
-	else if (el == element::Al) { return 40; }
+	else if (el == element::Ar) { return 40; }
+	else if (el == element::Si) { return 28; }
 	else if (el == element::Ti) { return 48; }
 	else if (el == element::Fe) { return 56; }
 	else if (el == element::Ni) { return 59; }
@@ -44,14 +61,39 @@ int get_atom_weight(element el)
 	else if (el == element::Au) { return 197;}
 }
 
-int get_atom_weight(char title)
+int get_atomic_mass(char title)
 {
-	return get_atom_weight(convert_to_element(title));
+	return get_atomic_mass(convert_to_element(title));
 }
 
-int get_atom_weight(char title[2])
+int get_atomic_mass(char title[2])
 {
-	return get_atom_weight(convert_to_element(title));
+	return get_atomic_mass(convert_to_element(title));
+}
+
+int get_atomic_mass(std::string title)
+{
+	return get_atomic_mass(convert_to_element(title));
+}
+
+int get_atomic_number(element el)
+{
+	return static_cast<int>(el);
+}
+
+int get_atomic_number(char title)
+{
+	return get_atomic_number(convert_to_element(title));
+}
+
+int get_atomic_number(char title[2])
+{
+	return get_atomic_number(convert_to_element(title));
+}
+
+int get_atomic_number(std::string title)
+{
+	return get_atomic_number(convert_to_element(title));
 }
 
 #endif	//TAHD_ELEMENTS_H
