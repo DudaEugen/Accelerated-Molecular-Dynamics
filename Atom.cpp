efb242850fceec902efe9276986b0f4fa0479d90
DudaEugen <54473424+DudaEugen@users.noreply.github.com>
2019-11-29 14:27:49 +0200

    create base realizarion Atom class except functions on acceleration

--- Atom.cpp
+++ Atom.cpp
@@ -1,1 +1,93 @@
 #include "Atom.h"
+
+bool Atom::isTransferedAccelerations = false;
+short unsigned int Atom::stepIndex = 0;
+unsigned int Atom::nextId = 0;
+
+Atom::Atom(element element, double coordinates[DIMENSIONAL_NUMBER])
+	: id(createId()), chemElement(element)
+{
+	constructing(coordinates);
+}
+
+Atom::Atom(char element, double coordinates[DIMENSIONAL_NUMBER])
+	: id(createId()), chemElement(convert_to_element(element))
+{
+	constructing(coordinates);
+}
+
+Atom::Atom(char element[2], double coordinates[DIMENSIONAL_NUMBER])
+	: id(createId()), chemElement(convert_to_element(element))
+{
+	constructing(coordinates);
+}
+
+Atom::Atom(std::string element, double coordinates[DIMENSIONAL_NUMBER])
+	: id(createId()), chemElement(convert_to_element(element))
+{
+	constructing(coordinates);
+}
+
+void Atom::computeMass() { mass = get_atomic_mass(chemElement) * 0.001 / N_AVOGADRO; }
+
+int Atom::createId() const 
+{ 
+	nextId++;
+	return nextId - 1;
+}
+
+void Atom::setCoordinates(double coordinates[DIMENSIONAL_NUMBER])
+{
+	for (short unsigned int i = 0; i < DIMENSIONAL_NUMBER; i++)
+	{
+		r[i] = coordinates[i];
+	}
+}
+
+void Atom::constructing(double coordinates[DIMENSIONAL_NUMBER])
+{
+	setCoordinates(coordinates);
+	computeMass();
+	for (short unsigned int i = 0; i < DIMENSIONAL_NUMBER; i++)
+	{
+		v[i] = 0;
+		aStream[i] = nullptr;
+		aSystem[0][i] = nullptr;
+		aSystem[1][i] = nullptr;
+	}
+}
+
+int Atom::getPreviousStepIndex() const { return  stepIndex == 0 ? 1 : 0; }
+void Atom::changeStepIndex() { stepIndex = stepIndex == 0 ? 1 : 0; }
+
+void Atom::setVelocity(double velocity[DIMENSIONAL_NUMBER])
+{
+	for (short unsigned int i = 0; i < DIMENSIONAL_NUMBER; i++)
+	{
+		v[i] = velocity[i];
+	}
+}
+
+void Atom::addVelocity(double adding_velocity[DIMENSIONAL_NUMBER])
+{
+	for (short unsigned int i = 0; i < DIMENSIONAL_NUMBER; i++)
+	{
+		v[i] += adding_velocity[i];
+	}
+}
+
+void Atom::setAccelerationStream(double acceleration[DIMENSIONAL_NUMBER])
+{
+	for (short unsigned int i = 0; i < DIMENSIONAL_NUMBER; i++)
+	{
+		*aStream[i] = acceleration[i];
+	}
+}
+
+void Atom::setAccelerationSystem(double acceleration[DIMENSIONAL_NUMBER])
+{
+	for (short unsigned int i = 0; i < DIMENSIONAL_NUMBER; i++)
+	{
+		*aSystem[stepIndex][i] = acceleration[i];
+	}
+}
--- Atom.h
+++ Atom.h
@@ -4,12 +4,38 @@
 
 #include "Elements.h"
 #include "constants.h"
+#include <string>
 #include <cmath>
 
 class Atom
 {
 private:
-	element cham_element;
+	static unsigned int nextId;
+	static short unsigned int stepIndex;		//indexing steps: in aSystem[stepIndex][] conteining actuality accelerations
+	int id;
+	element chemElement;
+	double mass;
+	double r[DIMENSIONAL_NUMBER];
+	double v[DIMENSIONAL_NUMBER];
+	double* aStream[DIMENSIONAL_NUMBER];		//accelerated containing here before accelerations transfer by MPI 
+	double* aSystem[2][DIMENSIONAL_NUMBER];		//accelerations are containing here after accelerations transfer by MPI. 
+												//Accelerations for previous step are containing in previous value of first index (stepIndex)
+	void computeMass();
+	int createId() const;
+	void constructing(double coordinates[DIMENSIONAL_NUMBER]);
+	int getPreviousStepIndex() const;
+	void setAccelerationStream(double acceleration[DIMENSIONAL_NUMBER]);
+	void setAccelerationSystem(double acceleration[DIMENSIONAL_NUMBER]);
+
+public:
+	Atom(element element, double coordinates[DIMENSIONAL_NUMBER]);
+	Atom(char element, double coordinates[DIMENSIONAL_NUMBER]);
+	Atom(char element[2], double coordinates[DIMENSIONAL_NUMBER]);
+	Atom(std::string element, double coordinates[DIMENSIONAL_NUMBER]);
+	static void changeStepIndex();
+	void setCoordinates(double coordinates[DIMENSIONAL_NUMBER]);
+	void setVelocity(double velocity[DIMENSIONAL_NUMBER]);
+	void addVelocity(double adding_velocity[DIMENSIONAL_NUMBER]);
 };
 
 #endif	//TAHD_ATOM_H
