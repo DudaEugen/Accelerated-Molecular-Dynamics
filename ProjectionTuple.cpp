3c74480883fdf5895a10fb6648aa9c33e919d568
PolyakAnton <54473424+PolyakAnton@users.noreply.github.com>
2019-12-02 16:16:55 +0200

    operators of ProjectionTuple class is overloaded

--- ProjectionTuple.cpp
+++ ProjectionTuple.cpp
@@ -1,8 +1,10 @@
 #include "ProjectionTuple.h"
 
+typedef unsigned short int index;
+
 ProjectionTuple::ProjectionTuple()
 {
-	for (unsigned short int i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
 	{
 		projections[i] = 0;
 	}
@@ -10,7 +12,7 @@ ProjectionTuple::ProjectionTuple()
 
 ProjectionTuple::ProjectionTuple(const double projectionArray[DIMENSIONAL_NUMBER])
 {
-	for (unsigned short int i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
 	{
 		projections[i] = projectionArray[i];
 	}
@@ -18,8 +20,93 @@ ProjectionTuple::ProjectionTuple(const double projectionArray[DIMENSIONAL_NUMBER
 
 ProjectionTuple::ProjectionTuple(const ProjectionTuple& projectionTuple)
 {
-	for (unsigned short int i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
 	{
 		projections[i] = projectionTuple.projections[i];
 	}
+}
+
+void ProjectionTuple::operator = (const ProjectionTuple &other)
+{
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	{
+		projections[i] = other.projections[i];
+	}
+}
+
+ProjectionTuple ProjectionTuple::operator + (const ProjectionTuple &other) const
+{
+	ProjectionTuple result = ProjectionTuple();
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	{
+		result.projections[i] = projections[i] + other.projections[i];
+	}
+	return result;
+}
+
+ProjectionTuple ProjectionTuple::operator - (const ProjectionTuple &other) const
+{
+	ProjectionTuple result = ProjectionTuple();
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	{
+		result.projections[i] = projections[i] - other.projections[i];
+	}
+	return result;
+}
+
+void ProjectionTuple::operator += (const ProjectionTuple &other)
+{
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	{
+		projections[i] += other.projections[i];
+	}
+}
+
+void ProjectionTuple::operator -= (const ProjectionTuple &other)
+{
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	{
+		projections[i] -= other.projections[i];
+	}
+}
+
+void ProjectionTuple::operator *= (const double factor)
+{
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	{
+		projections[i] *= factor;
+	}
+}
+
+void ProjectionTuple::operator /= (const double divider)
+{
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	{
+		projections[i] /= divider;
+	}
+}
+
+const ProjectionTuple operator * (const ProjectionTuple &projectionTuple, const double factor)
+{
+	ProjectionTuple result = ProjectionTuple();
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	{
+		result.projections[i] = projectionTuple.projections[i] * factor;
+	}
+	return result;
+}
+
+const ProjectionTuple operator * (const double factor, const ProjectionTuple &projectionTuple) 
+{
+	return projectionTuple * factor;
+}
+
+ProjectionTuple ProjectionTuple::operator / (const double divider) const
+{
+	ProjectionTuple result = ProjectionTuple();
+	for (index i = 0; i < DIMENSIONAL_NUMBER; ++i)
+	{
+		result.projections[i] = projections[i] / divider;
+	}
+	return result;
 }
--- ProjectionTuple.h
+++ ProjectionTuple.h
@@ -11,7 +11,17 @@ private:
 public:
 	ProjectionTuple();
 	ProjectionTuple(const double projectionArray[DIMENSIONAL_NUMBER]);
-	ProjectionTuple(const ProjectionTuple& projectionTuple);
+	ProjectionTuple(const ProjectionTuple &projectionTuple);
+	void operator = (const ProjectionTuple &other);
+	ProjectionTuple operator + (const ProjectionTuple &other) const;
+	ProjectionTuple operator - (const ProjectionTuple &other) const;
+	void operator += (const ProjectionTuple &other);
+	void operator -= (const ProjectionTuple &other);
+	void operator *= (const double factor);
+	void operator /= (const double divider);
+	ProjectionTuple operator / (const double divider) const;
+	friend const ProjectionTuple operator * (const ProjectionTuple &projectionTuple, const double factor);
+	friend const ProjectionTuple operator * (const double factor, const ProjectionTuple &projectionTuple);
 };
 
 #endif	//TAHD_PROJECTION_TUPLE_H
