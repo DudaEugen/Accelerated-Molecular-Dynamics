3d84c6d5ef0d5258539262a46c34399c4a5bdd82
DudaEugen <54473424+DudaEugen@users.noreply.github.com>
2020-01-30 12:00:40 +0200

    dissipativeLeyers for AThermostat is public now

--- Thermostat/AThermostat.cpp
+++ Thermostat/AThermostat.cpp
@@ -8,16 +8,6 @@ void AThermostat::setTemperature(const double temperature)
 	this->T0 = temperature;
 }
 
-void AThermostat::setAtomList(std::vector<Atom>& atoms)
-{
-	size_t size = atoms.size();
-	atomsDissipativeLeyers.clearList();
-	for (size_t i = 0; i < size; ++i)
-	{
-		atomsDissipativeLeyers.addAtom(&atoms[i]);
-	}
-}
-
 double AThermostat::computeTemperature()
 {
 	T = 0;
--- Thermostat/AThermostat.h
+++ Thermostat/AThermostat.h
@@ -10,13 +10,10 @@ class AThermostat
 protected:
 	double T0;							//Temperature of thermostat
 	double T;							//Temperature of System
-	AtomGroup atomsDissipativeLeyers;	//if Thermostat using dissipative leyers
-	std::vector<Atom>& atomsSystem;		//pointer to all moving atoms of System
 public:
+	AtomGroup dissipativeLeyers;
 	virtual ~AThermostat() = 0;
 	void setTemperature(const double temperature);
-	/*For using instead of AtomGroup property in children classes if necessary*/
-	virtual void setAtomList(std::vector<Atom>& atoms);
 	double computeTemperature();
 	virtual void heatExchange() = 0;
 };
