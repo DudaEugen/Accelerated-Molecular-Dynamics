f3e826c2f031d877a1809b46270d9e48edafaec7
DudaEugen <54473424+DudaEugen@users.noreply.github.com>
2020-06-04 12:09:53 +0300

    the responsibility for calling the method changeStepIndex now lies with the class Atom - stepIndex is not static field now - the responsibility for calling the doStepEuler for first step now lies with the method move + method addAcceleration is fixed

--- Atom.cpp
+++ Atom.cpp
@@ -1,8 +1,8 @@
 #include "Atom.h"
 
-short unsigned int Atom::stepIndex = 0;
-
 Atom::Atom(const element element, Vector::ConstVectorPass coordinates):
+	isFirstStepTaken{ false },
+	actualStepIndex{ 0 },
 	chemElement{ element }, 
 	mass{ computeMass(element) },
 	r{ coordinates }, 
@@ -25,34 +25,53 @@ Atom::Atom(const std::string element, Vector::ConstVectorPass coordinates) : Ato
 
 double Atom::computeMass(const element element) { return get_atomic_mass(element) * 0.001 / N_AVOGADRO; }
 
-int Atom::getPreviousStepIndex() const { return  stepIndex == 0 ? 1 : 0; }
+char Atom::getPreviousStepIndex()
+{
+	return actualStepIndex == 0 ? 1 : 0;
+}
 
-void Atom::changeStepIndex() { stepIndex = stepIndex == 0 ? 1 : 0; }
+void Atom::changeStepIndex() 
+{ 
+	actualStepIndex = actualStepIndex == 0 ? 1 : 0;
+}
+
+void Atom::doStepEuler(const double dt)
+{
+	r += v * dt + 0.5 * a[actualStepIndex] * pow(dt, 2);
+	v += a[actualStepIndex] * dt;
+}
+
+void Atom::doStepVelocityVerlet(const double dt)
+{
+	r += v * dt + 0.5 * a[actualStepIndex] * pow(dt, 2);
+	v += 0.5 * (a[actualStepIndex] + a[getPreviousStepIndex()]) * dt;
+}
 
 void Atom::setCoordinates(Vector::ConstVectorPass coordinates) { r = coordinates; }
 
 void Atom::setVelocity(Vector::ConstVectorPass velocity) { v = velocity; }
 
-void Atom::setAcceleration(Vector::ConstVectorPass acceleration) { a[stepIndex] = acceleration; }
+void Atom::setAcceleration(Vector::ConstVectorPass acceleration) { a[actualStepIndex] = acceleration; }
 
 void Atom::addVelocity(Vector::ConstVectorPass addingVelocity) { v += addingVelocity; }
 
-void Atom::addAcceleration(Vector::ConstVectorPass addingAcceleratrion) { a[stepIndex] = addingAcceleratrion; }
+void Atom::addAcceleration(Vector::ConstVectorPass addingAcceleration) { a[actualStepIndex] += addingAcceleration; }
 
 Vector::ConstVectorPass Atom::getCoordinates() const { return r; }
 
 Vector::ConstVectorPass Atom::getVelocity() const { return v; }
 
-Vector::ConstVectorPass Atom::getAcceleration() const { return a[stepIndex]; }
+Vector::ConstVectorPass Atom::getAcceleration() const { return a[actualStepIndex]; }
 
-void Atom::doStepEuler(const double dt)
+void Atom::move(const double dt)
 {
-	r += v * dt + 0.5 * a[stepIndex] * pow(dt, 2);
-	v += a[stepIndex] * dt;
-}
-
-void Atom::doStepVelocityVerlet(const double dt)
-{
-	r += v * dt + 0.5 * a[stepIndex] * pow(dt, 2);
-	v += 0.5 * (a[stepIndex] + a[getPreviousStepIndex()]) * dt;
+	if (isFirstStepTaken)
+	{
+		doStepVelocityVerlet(dt);
+	}
+	else
+	{
+		doStepEuler(dt);
+	}
+	changeStepIndex();
 }
--- Atom.h
+++ Atom.h
@@ -11,8 +11,10 @@
 class Atom
 {
 private:
-	static short unsigned int stepIndex;		/*indexing steps: a[stepIndex] containing actuality accelerations
-												  a[not stepIndex] containing accelerations corresponding previous step*/
+	bool isFirstStepTaken;
+	char actualStepIndex;						/*indexing steps: a[actualStepIndex] containing actuality accelerations
+												  a[not actualStepIndex] containing accelerations corresponding previous step*/
+
 public:
 	const element chemElement;
 	const double mass;
@@ -22,14 +24,16 @@ private:
 	Vector a[2];
 
 	double computeMass(const element element);
-	int getPreviousStepIndex() const;
+	char getPreviousStepIndex();
+	void changeStepIndex();
+	void doStepEuler(const double dt);
+	void doStepVelocityVerlet(const double dt);	//dont first step
 public:
 	Atom(const element element, Vector::ConstVectorPass coordinates);
 	Atom(const char element, Vector::ConstVectorPass coordinates);
 	Atom(const char element[2], Vector::ConstVectorPass coordinates);
 	Atom(const std::string element, Vector::ConstVectorPass coordinates);
-	static void changeStepIndex();				/*the class invoking doStepVelocityVerlet method is responsible for calling changeStepIndex()
-												  after ever call doStepVelocityVerlet for all atoms*/
+					
 	void setCoordinates(Vector::ConstVectorPass coordinates);
 	void setVelocity(Vector::ConstVectorPass velocity);
 	void setAcceleration(Vector::ConstVectorPass acceleration);
@@ -40,8 +44,7 @@ public:
 	Vector::ConstVectorPass getVelocity() const;
 	Vector::ConstVectorPass getAcceleration() const;
 												//doStepAlhorithm are methods to moving atom corresponding algorithm
-	void doStepEuler(const double dt);
-	void doStepVelocityVerlet(const double dt);	//dont first step
+	void move(const double dt);
 };
 
 #endif	//TAHD_ATOM_H
