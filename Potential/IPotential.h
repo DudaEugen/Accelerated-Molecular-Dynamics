2e70a981e4ebf4bd03c1f6fb63cca60e47ae8f71
DudaEugen <54473424+DudaEugen@users.noreply.github.com>
2020-06-17 12:56:03 +0300

    create interface for potentials

---
+++ Potential/IPotential.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef TAHD_IPOTENTIAL_H
+#define TAHD_IPOTENTIAL_H
+
+#include <vector>
+#include "../AtomPair.h"
+
+class IPotential
+{
+public:
+	virtual ~IPotential() = 0;
+	virtual void computeAndSetAccelerations(const std::vector<AtomPair>& pairs) = 0;
+	virtual double getCutRadius() = 0;
+};
+
+#endif	//TAHD_IPOTENTIAL_H
