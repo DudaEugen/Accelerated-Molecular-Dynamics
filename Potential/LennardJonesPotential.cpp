1df00d1cf078663e7241eebc4a466766cf1f782c
DudaEugen <54473424+DudaEugen@users.noreply.github.com>
2020-06-23 13:02:05 +0300

    add Lennard-Jones potential

---
+++ Potential/LennardJonesPotential.cpp
@@ -0,0 +1,37 @@
+#include "LennardJonesPotential.h"
+
+LennardJonesPotential::LennardJonesPotential(const std::size_t maxAtomPairTypes)
+	: APairPotential{ maxAtomPairTypes }
+{
+}
+
+LennardJonesPotential::~LennardJonesPotential() {}
+
+void LennardJonesPotential::addPairType(element first, element second, double bondEnergy, double bondRadius, double cutRadius)
+{
+	if (APairPotential::addPairType(first, second))
+	{
+		eps.push_back(bondEnergy);
+		rm.push_back(bondRadius);
+		rc.push_back(cutRadius);
+	}
+}
+
+void LennardJonesPotential::computeAndSetAccelerations()
+{
+	Vector force;
+	for (std::size_t i = 0; i < atomPairs->size(); ++i)
+	{
+		double distance = (*atomPairs)[i].getDistance();
+		if (distance < rc[i])
+		{
+			force = 24 * Q_ELEMENTARY * 0.0001 * eps[i] *
+				( 1 - 2*pow(rm[i]/distance, 6) ) * pow(rm[i] / distance, 8) / std::pow(rm[i], 2) *
+				(*atomPairs)[i].getDistanceProjections();
+
+			(*atomPairs)[i].getFirst().addAcceleration(force / (*atomPairs)[i].getFirst().mass);
+			if ((*atomPairs)[i].getIsAtomsFromSameStream())
+				(*atomPairs)[i].getSecond().addAcceleration(-force / (*atomPairs)[i].getSecond().mass);
+		}
+	}
+}
---
+++ Potential/LennardJonesPotential.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef TAHD_MORZE_POTENTIAL_H
+#define TAHD_MORZE_POTENTIAL_H
+
+#include "APairPotential.h"
+
+class LennardJonesPotential : public APairPotential
+{
+	std::vector<double> eps;
+	std::vector<double> rm;
+public:
+	LennardJonesPotential(const std::size_t maxAtomPairTypes);
+	~LennardJonesPotential() override;
+	void addPairType(element first, element second, double bondEnergy, double bondRadius, double cutRadius);
+	// bondEnergy(eV), bondRadius(A), cutradius(A)
+
+	void computeAndSetAccelerations() override;
+};
+
+#endif	//TAHD_MORZE_POTENTIAL_H
