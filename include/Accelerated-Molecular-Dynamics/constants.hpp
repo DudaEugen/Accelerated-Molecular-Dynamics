37bb11c62cd058daf84edec30d829f0f5b938f9f
PolyakAnton <54473424+PolyakAnton@users.noreply.github.com>
2020-11-22 19:34:34 +0200

    add inline to consts in constants.hpp

--- include/Accelerated-Molecular-Dynamics/constants.hpp
+++ include/Accelerated-Molecular-Dynamics/constants.hpp
@@ -7,7 +7,7 @@
 
 #define DIMENSIONAL_NUMBER 3
 #define MAX_DIMENSIONAL_FOR_VALUE_PASSING 3
-constexpr double PI = 3.14159265359;
+
 //physical constants
 constexpr double power(unsigned int v1, int v2)
 {
@@ -25,9 +25,9 @@ constexpr double power(unsigned int v1, int v2)
 	}
 }
 
-constexpr double K_BOLTZMANN = 1.38064852 * power(10, -23);
-constexpr double N_AVOGADRO = 6.02214129 * power(10, 23);
-constexpr double Q_ELEMENTARY = 1.60217662 * power(10, -19);
-constexpr double H_PLANCK = 6.62606957 * power(10, -34);
+inline constexpr double K_BOLTZMANN = 1.38064852 * power(10, -23);
+inline constexpr double N_AVOGADRO = 6.02214129 * power(10, 23);
+inline constexpr double Q_ELEMENTARY = 1.60217662 * power(10, -19);
+inline constexpr double H_PLANCK = 6.62606957 * power(10, -34);
 
 #endif	//TAHD_CONSTANTS_H
