cd9a2b5a77bacc145afeb209d2e0f972a0386972
DudaEugen <duda.evgen@gmail.com>
2022-04-03 17:17:58 +0300

    small changes in Vector.cpp

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -20,9 +20,10 @@ md::Vector::Vector(const double projectionArray[kDimensionalNumber]) noexcept
 
 md::Vector::Vector(const std::initializer_list<double>& init_list)
 {
+	auto begin = init_list.begin();
 	for (projection_index i = 0; i < kDimensionalNumber; ++i)
 	{
-		projections[i] = init_list.size() > i ? *(init_list.begin() + i) : 0;
+		projections[i] = init_list.size() > i ? *(begin + i) : 0;
 	}
 }
 
@@ -34,7 +35,10 @@ md::Vector::Vector(const Vector& other) noexcept
 	}
 }
 
-md::Vector::projection_index md::Vector::size() const noexcept { return kDimensionalNumber; }
+md::Vector::projection_index md::Vector::size() const noexcept
+{
+	return kDimensionalNumber;
+}
 
 md::Vector& md::Vector::operator = (ConstPass other) noexcept
 {
@@ -136,14 +140,32 @@ double md::Vector::absoluteValue() const noexcept
 	return std::sqrt(sumSquares());
 }
 
-md::Vector::iterator md::Vector::begin() noexcept { return projections.begin(); }
+md::Vector::iterator md::Vector::begin() noexcept
+{
+	return projections.begin();
+}
 
-md::Vector::iterator md::Vector::end() noexcept { return projections.end(); }
+md::Vector::iterator md::Vector::end() noexcept
+{
+	return projections.end();
+}
 
-md::Vector::const_iterator md::Vector::begin() const noexcept { return projections.begin(); }
+md::Vector::const_iterator md::Vector::begin() const noexcept
+{
+	return projections.begin();
+}
 
-md::Vector::const_iterator md::Vector::end() const noexcept { return projections.end(); }
+md::Vector::const_iterator md::Vector::end() const noexcept
+{
+	return projections.end();
+}
 
-md::Vector::const_iterator md::Vector::cbegin() const noexcept { return projections.cbegin(); }
+md::Vector::const_iterator md::Vector::cbegin() const noexcept
+{
+	return projections.cbegin();
+}
 
-md::Vector::const_iterator md::Vector::cend() const noexcept { return projections.cend(); }
+md::Vector::const_iterator md::Vector::cend() const noexcept
+{
+	return projections.cend();
+}
