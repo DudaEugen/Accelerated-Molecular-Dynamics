80eccbdcbe112a9f17d381064c6e9e360ef6de2c
DudaEugen <duda.evgen@gmail.com>
2022-06-25 17:56:44 +0300

    refactor testAtomPair

--- tests/tests/testAtomPair.cpp
+++ tests/tests/testAtomPair.cpp
@@ -3,27 +3,67 @@
 
 using namespace md;
 
+namespace testAtomPairMethods
+{
+	void testConstructor()
+	{
+		Atom a("Cu", randomPosition());
+		Atom b("Al", randomPosition());
+		AtomPair pair(a, b);
+
+		assert(&a == &pair.getFirst());
+		assert(&b == &pair.getSecond());
+	}
+
+	void testGetDistanceWithProjections()
+	{
+		Atom a("Cu", randomPosition());
+		Atom b("Cu", randomPosition());
+		AtomPair pair(a, b);
+
+		Vector expectedProjections = b.getPosition() - a.getPosition();
+		auto [distance, projections] = pair.getDistanceWithProjections();
+		assert(equal(projections, expectedProjections));
+		assert(equal(distance, expectedProjections.absoluteValue()));
+		assert(equal(pair.getDistance(), expectedProjections.absoluteValue()));
+		assert(equal(pair.getProjections(), expectedProjections));
+	}
+
+	void testComputeDistanceWithProjections()
+	{
+		Atom a("Cu", randomPosition());
+		Atom b("Cu", randomPosition());
+		AtomPair pair(a, b);
+
+		b.setPosition(randomPosition());
+		Vector expectedProjections = b.getPosition() - a.getPosition();
+
+		auto [oldDistance, oldProjections] = pair.getDistanceWithProjections();
+		assert(!equal(oldProjections, expectedProjections));
+		assert(!equal(oldDistance, expectedProjections.absoluteValue()));
+
+		pair.computeDistanceWithProjections();
+		auto [distance, projections] = pair.getDistanceWithProjections();
+		assert(equal(projections, expectedProjections));
+		assert(equal(distance, expectedProjections.absoluteValue()));
+	}
+
+	void testZeroDistance()
+	{
+		Atom a("Cu", randomPosition());
+		Atom b("Cu", a.getPosition());
+		AtomPair pair(a, b);
+
+		auto [distance, projections] = pair.getDistanceWithProjections();
+		assert(equal(projections, Vector()));
+		assert(equal(distance, 0));
+	}
+}
+
 void testAtomPair()
 {
-    Atom a("Cu", randomPosition());
-	Atom b("Al", randomPosition());
-	AtomPair pair(a, b);
-
-	assert(&a == &pair.getFirst());
-	assert(&b == &pair.getSecond());
-	Vector d = b.getPosition() - a.getPosition();
-	auto [distance, projections] = pair.getDistanceWithProjections();
-	assert(equal(d, projections));
-	assert(equal(d.absoluteValue(), distance));
-
-	b.setPosition(randomPosition());
-	auto [distance2, projections2] = pair.computeDistanceWithProjections();
-	d = b.getPosition() - a.getPosition();
-	assert(equal(d, projections2));
-	assert(equal(d.absoluteValue(), distance2));
-
-	b.setPosition(a.getPosition());
-	auto [distance3, projections3] = pair.computeDistanceWithProjections();
-	assert(equal(distance3, 0));
-	assert(equal(projections3, Vector()));
+    testAtomPairMethods::testConstructor();
+	testAtomPairMethods::testGetDistanceWithProjections();
+	testAtomPairMethods::testComputeDistanceWithProjections();
+	testAtomPairMethods::testZeroDistance();
 }
