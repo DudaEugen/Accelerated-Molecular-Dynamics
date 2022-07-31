956bf89661488e1580fb8a361cf9e5ded6d5947b
DudaEugen <duda.evgen@gmail.com>
2022-07-31 21:05:11 +0300

    refactor Atomic System

--- src/System/AtomicSystem.cpp
+++ src/System/AtomicSystem.cpp
@@ -1,10 +1,79 @@
+#include <algorithm>
 #include "System/AtomicSystem.hpp"
 
-md::AtomicSystem::AtomicSystem(const std::vector<Atom>& allAtoms, IPotential* atomicPotential)
-    : atoms{ allAtoms }, potential{ atomicPotential }
+md::AtomicSystem::AtomicSystem(
+	const std::vector<Atom>& allAtoms,
+	IPotential* atomicPotential,
+	AThermostat* thermostat
+) : AtomicSystem{allAtoms, std::vector{atomicPotential}, thermostat}
 {
 }
 
-std::vector<md::AtomPair>& md::AtomicSystem::getAtomPairs() noexcept { return atomPairs; }
+md::AtomicSystem::AtomicSystem(
+	const std::vector<Atom>& allAtoms,
+	std::vector<IPotential*> potentials,
+	AThermostat* thermostat
+) : 
+    atoms{allAtoms},
+    potentials{potentials},
+    neighboursList{
+        atoms,
+        (*std::min_element(
+            potentials.begin(),
+            potentials.end(),
+            [](const IPotential *a, const IPotential *b){ return a->getCutRadius() < b->getCutRadius(); }
+        ))->getCutRadius(),
+        1.2 * (*std::min_element(
+            potentials.begin(),
+            potentials.end(),
+            [](const IPotential *a, const IPotential *b){ return a->getCutRadius() < b->getCutRadius(); }
+        ))->getCutRadius()
+    },
+    thermostat{thermostat}
+{
+}
 
-std::vector<md::Atom>& md::AtomicSystem::getAtoms() noexcept { return atoms; }
+md::AtomicSystem::~AtomicSystem()
+{
+    delete thermostat;
+}
+
+void md::AtomicSystem::run(double time, double timeStep)
+{
+    double cutRadius = (*std::min_element(
+        potentials.begin(),
+        potentials.end(),
+        [](const IPotential *a, const IPotential *b){ return a->getCutRadius() < b->getCutRadius(); }
+    ))->getCutRadius();
+    Vector cellSize = neighboursList.getCellSize();
+    double minCellSize = *std::min_element(cellSize.begin(), cellSize.end());
+
+    double timeForNeighboursRefresh = -1;
+    for (double currentTime = 0; currentTime < time; currentTime += timeStep)
+    {
+        if (timeForNeighboursRefresh < 0)
+        {
+            neighboursList.refresh(atoms);
+            double maxVelocity = (*std::max_element(
+                atoms.begin(),
+                atoms.end(),
+                [](const Atom& first, const Atom& second)
+                {
+                    return first.getVelocity().absoluteValue() < second.getVelocity().absoluteValue();
+                }
+            )).getVelocity().absoluteValue();
+            timeForNeighboursRefresh = (minCellSize - cutRadius) / (2 * maxVelocity);
+        }
+        timeForNeighboursRefresh -= timeStep;
+        for (auto potential: potentials)
+        {
+            potential->computeAndSetAccelerations(neighboursList.getPairs());
+        }
+        // TODO: MPI;
+        // TODO: use thermostat
+        for (auto& atom: atoms)
+        {
+            atom.move(timeStep);
+        }
+    }
+}
