5e3c2d51bff592fee19e41006056abe27ed9de26
DudaEugen <duda.evgen@gmail.com>
2022-08-14 15:57:56 +0300

    refactor Parallels

---
+++ include/AcceleratedMolecularDynamics/Parallel/MPI/Processes.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef TAHD_PROCESSES_H
+#define TAHD_PROCESSES_H
+
+#include <vector>
+#include "Atoms/Atom.hpp"
+
+namespace md
+{
+    class Processes
+    {
+    private:
+        const int count;
+        const int rank;
+
+        static int defineCount();
+        static int defineRank();
+
+        void broadcast(std::vector<double>& data) const;
+        void gatherToAll(std::vector<double>& data, std::vector<int>& sendCounts) const;
+
+    public:
+        Processes();
+        ~Processes() = default;
+
+        unsigned getCount() const noexcept;
+        unsigned getRank() const noexcept;
+
+        void setVelocities(std::vector<Atom>& atoms) const;
+        void exchangeAccelerations(std::vector<Atom>& atoms, const std::vector<int>& sendCounts) const;
+    };
+}
+
+#endif // TAHD_ATOM_GROUP_H
