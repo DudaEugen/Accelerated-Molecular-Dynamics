455eaa8134a6961c97e5018ff2562c72f9521d7a
DudaEugen <duda.evgen@gmail.com>
2022-11-13 12:16:21 +0200

    Create APotentialEAM class

---
+++ include/AcceleratedMolecularDynamics/Potential/EAM/APotentialEAM.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef TAHD_ABSTRACT_POTENTIAL_EAM_H
+#define TAHD_ABSTRACT_POTENTIAL_EAM_H
+
+#include <functional>
+#include "Potential/PairPotential/APairPotential.hpp"
+
+namespace md
+{
+	class APotentialEAM: public APotential
+	{
+	protected:
+        class AEmbeddingTerm
+        {
+            double cutRadius;
+            std::pair<element, element> elements;
+        public:
+            AEmbeddingTerm(element first, element second, double cutRadius) noexcept;
+            virtual ~AEmbeddingTerm() = 0;
+
+            bool isCorrectElements(const AtomPair& atomPair) const noexcept;
+            double getCutRadius() const noexcept;
+            virtual double computeTerm(const AtomPair& atomPair) const = 0;
+            virtual Vector computeTermDerivative(const AtomPair& atomPair) const = 0;
+        };
+
+		APairPotential const * const pairTerm;
+        const std::vector<const AEmbeddingTerm*> embeddingTerms;
+
+        virtual std::function<double(double)> embeddingFunctionDerivative() const = 0;
+	public:
+		APotentialEAM(
+            APairPotential* pairPotential,
+            const std::vector<const AEmbeddingTerm*>& terms
+        ) noexcept;
+		~APotentialEAM();
+        void addAccelerations(NeighboursList& neighboursList) const override;
+		double getCutRadius() const noexcept override;
+	};
+}
+
+#endif	//TAHD_ABSTRACT_POTENTIAL_EAM_H
---
+++ src/Potential/EAM/APotentialEAM.cpp
@@ -0,0 +1,108 @@
+#include <algorithm>
+#include <unordered_map>
+#include "Potential/EAM/APotentialEAM.hpp"
+
+md::APotentialEAM::AEmbeddingTerm::AEmbeddingTerm(
+    element first, element second, double cutRadius
+) noexcept
+    : elements{first, second}, cutRadius{cutRadius}
+{
+}
+
+bool md::APotentialEAM::AEmbeddingTerm::isCorrectElements(const AtomPair& atomPair) const noexcept
+{
+    return (
+		(
+			atomPair.getFirst().chemElement == elements.first &&
+			atomPair.getSecond().chemElement == elements.second
+		) || (
+			atomPair.getFirst().chemElement == elements.second &&
+			atomPair.getSecond().chemElement == elements.first
+		)
+	);
+}
+
+double md::APotentialEAM::AEmbeddingTerm::getCutRadius() const noexcept
+{
+    return cutRadius;
+}
+
+md::APotentialEAM::APotentialEAM(
+    APairPotential* pairPotential,
+    const std::vector<const AEmbeddingTerm*>& terms
+) noexcept
+    : pairTerm{ pairPotential }, embeddingTerms{ terms }
+{
+}
+
+md::APotentialEAM::~APotentialEAM()
+{
+    delete pairTerm;
+    for (auto term: embeddingTerms)
+    {
+        delete term;
+    }
+}
+
+double md::APotentialEAM::getCutRadius() const noexcept
+{
+    auto maxEmbeddingCutRadius = (*std::max_element(
+        embeddingTerms.cbegin(),
+        embeddingTerms.cend(),
+        [](const AEmbeddingTerm* first, const AEmbeddingTerm* second)
+        {
+            return first->getCutRadius() < second->getCutRadius();
+        }
+    ))->getCutRadius();
+    return std::max(maxEmbeddingCutRadius, pairTerm->getCutRadius());
+}
+
+void md::APotentialEAM::addAccelerations(NeighboursList& neighboursList) const
+{
+    auto embeddingAtoms = std::unordered_map<
+        Atom*, std::vector<std::pair<AtomPair*, const APotentialEAM::AEmbeddingTerm*>>
+    >();
+    for (auto& atomPair: neighboursList.getPairs())
+    {
+        for (auto embeddingTerm: embeddingTerms)
+        {
+            if (embeddingTerm->isCorrectElements(atomPair))
+            {
+                auto pair = std::pair(&atomPair, embeddingTerm);
+                embeddingAtoms[&atomPair.getFirst()].push_back(pair);
+                embeddingAtoms[&atomPair.getSecond()].push_back(pair);
+            }
+        }
+    }
+
+    auto embeddingFunctionDerivatives = std::unordered_map<Atom*, double>();
+    for (auto& [atom, pairs] : embeddingAtoms)
+    {
+        double summ = 0;
+        for (auto pair: pairs)
+        {
+            summ += pair.second->computeTerm(*pair.first);
+        }
+        embeddingFunctionDerivatives[atom] = embeddingFunctionDerivative()(summ);
+    }
+
+    for (auto& [atom, pairs] : embeddingAtoms)
+    {
+        for (auto pair: pairs)
+        {
+            Vector derivative = pair.second->computeTermDerivative(*pair.first);
+            Atom* anotherAtom = 
+                &pair.first->getFirst() == atom ? &pair.first->getSecond() : &pair.first->getFirst();
+            if (&pair.first->getFirst() != atom)
+            {
+                derivative *= -1;
+            }
+            atom->addAcceleration(derivative / (embeddingFunctionDerivatives[atom] * atom->mass));
+            anotherAtom->addAcceleration(
+                -derivative / (embeddingFunctionDerivatives[atom] * anotherAtom->mass)
+            );
+        }
+    }
+
+    pairTerm->addAccelerations(neighboursList);
+}
